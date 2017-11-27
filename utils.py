a4aaa9d42fd74edf6cded1c2716e55c888f418a4
Romain Madar <romain.madar@gmail.com>
2017-11-28 00:17:57 +0100

    Implementing ROC curve comparison and different naive training

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy             as     np
+import pandas            as     pd
 import matplotlib.pyplot as     plt
 import matplotlib        as     mpl
 from   sklearn.metrics   import roc_curve
